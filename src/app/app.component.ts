c69bb6314d2579c5dc3144b2f345d7006a9745bd
FabiánV03 <2021030292@upsin.edu.mx>
2024-08-16 13:17:09 -0700

    ContrasSeguras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CrearContrasSeguras';
+  password: string = '';
+  length: number = 12; // Longitud predeterminada de la contraseña
+
+  generatePassword(): void {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:",.<>?/';
+    let password = '';
+    for (let i = 0; i < this.length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    this.password = password;
+  }
+
+  onGeneratePassword(): void {
+    this.generatePassword();
+  }
+
+  getButtonClass(): string {
+    if (this.length == 8) {
+      return 'btn-short';
+    } else if (this.length <= 11) {
+      return 'btn-medium';
+    } else {
+      return 'btn-long';
+    }
+  }
 }
